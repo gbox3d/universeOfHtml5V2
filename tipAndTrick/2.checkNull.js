c8b5adca6d9f408e5e5856dacc37a52476ba5194
gbox3d <gbox3d@gmail.com>
2019-09-22 12:55:38 +0900

    함수이름 널체크 코드 스니핏

--- tipAndTrick/2.checkNull.js
+++ tipAndTrick/2.checkNull.js
@@ -1,19 +1,18 @@
-var testCheck = {
-  header : {
-    major : 100,
-    minor : 20
-  }
-}
+let testCheck = {
+  header: {
+    major: 100,
+    minor: 20
+  },
+  callback : _=>console.log(_)
+};
 
 /*
 js 구조체의 무결성을 순차적으로 검사해주는 방법
-하위 멤버가 undefined 이더라도 왼쪽부터 들어가다 undefined가 나오면 멈추기 때문에 예외상황이 발생하지않는다.
-
-상위 멤버 부터 좌측부터 써내려가지않고 만약 거꾸로 쓴다면 예외가 발생한다.
+왼쪽부터 체크되므로 undefined가 나오면 멈추기 때문에 예외상황이 발생하지않는다.
+좌측부터 써내려가지않고 만약 거꾸로 쓴다면 예외가 발생한다.
 
 예> testCheck.headers.minor && testCheck && testCheck.headers
-
- */
+*/
 
 testCheck && console.log('testCheck ok')
 testCheck && testCheck.header && console.log('testCheck.header ok')
@@ -26,3 +25,9 @@ if(testCheck && testCheck.headers && testCheck.headers.minor ) {
 else {
   console.log('testCheck.headers undefined');
 }
+
+/*
+함수가 undefined 인지 검사 하여 호출여부를 결정한다.
+ */
+testCheck.callback && testCheck.callback('back')
+testCheck.callfront && testCheck.callfront('front')
