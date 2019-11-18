86a1870f855bd204f7d3e3d5119b738647e142f6
gbox3d <gbox3d@gmail.com>
2019-11-18 18:19:22 +0900

    es6 예베 추가

---
+++ advJS/es6/foo.js
@@ -0,0 +1,16 @@
+
+//export 를 사용한다.
+function foo () {
+  console.log('it is foo')
+
+  let _elm = document.createElement('p')
+  _elm.innerText = 'hello es6'
+
+  document.body.appendChild(_elm)
+
+
+}
+var bar = "it is bar";
+var kaa = "it is karrr";
+
+export {foo,bar,kaa}
