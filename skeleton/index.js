06f66c11ddf5aecb85209d775865c9938f5887b4
gbox3d <gbox3d@gmail.com>
2021-05-20 14:59:54 +0900

    스켈레톤 프레임웍 URL객체 사용으로 수정

--- skeleton/index.js
+++ skeleton/index.js
@@ -1,22 +1,18 @@
 'use strict';
 
-const os = require('os');
-const nodeStatic = require('node-static');
-const socketIO = require('socket.io');
-//import UrlParser from "url"
-const UrlParser = require('url');
-
-const http = require('http');
-const fs = require('fs');
+import nodeStatic from 'node-static';
+import { URL } from 'url';
+import http from 'http';
+import fs from 'fs';
 
 const server_port = process.argv[2] ? parseInt(process.argv[2]) : 8000
 
 var fileServer = new (nodeStatic.Server)('./web');
 let serverApp = http.createServer((req, res) => {
 
-    var urlObj = UrlParser.parse(req.url, true);
+    // console.log(req.url);
 
-    if (urlObj.pathname.indexOf('/rest/') == 0) {
+    if (req.url.indexOf('/rest/') == 0) {
         //레스트 콜이면 
         var method = req.method;
         if (method == 'OPTIONS') { //post 처리 s
@@ -28,29 +24,10 @@ let serverApp = http.createServer((req, res) => {
 
         switch (method) {
             case 'GET':
-                {
-                    let header = {
-                        'Content-Type': 'text/plain',
-                        'Access-Control-Allow-Origin': '*',
-                        'Access-Control-Allow-Methods': 'GET',
-                        'Access-Control-Max-Age': '1000',
-                        "Access-Control-Allow-Headers": "*" //CORS 정책 허용  * 는 모두 허용 
-                    }
-
-                    if (urlObj.pathname == '/rest/hello') {
-                        res.writeHead(200, header);
-                        res.end(JSON.stringify({ r: 'ok',msg:'you say hello' }));
-
-                    }
-                    else {
-                        res.writeHead(200, header);
-                        res.end(JSON.stringify({ r: 'ok' }));
-                    }
-                }
-                // process_get(req, res);
+                process_get(req, res);
                 break;
             case 'POST':
-                // process_post(req, res);
+                process_post(req, res);
                 break;
         }
     }
@@ -62,5 +39,88 @@ let serverApp = http.createServer((req, res) => {
 }).listen(server_port, () => {
     console.log(`'server start port ${server_port}'`)
 });
+
+async function process_get(req, res) {
+
+    let urlObj = new URL(
+        req.url, // url이 상대적인 경로일경우(path만존재) 두번째 인자인 base url을 꼭 지정해주어야한다.
+        'http://example.org/' //base url (The base URL to resolve against if the input is not absolute.)
+    )
+
+    console.log(urlObj.searchParams.get('name'));
+    let _name = urlObj.searchParams.get('name')
+    let _number = parseInt(urlObj.searchParams.get('number'));
+
+    let header = {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Max-Age': '1000',
+        "Access-Control-Allow-Headers": "*" //CORS 정책 허용  * 는 모두 허용 
+    }
+
+    let _rest_path = urlObj.pathname.substring(5)
+
+    switch (_rest_path) {
+        case '/get/hello':
+            res.writeHead(200, header);
+            res.end(JSON.stringify({ r: 'ok', msg: `you(${_name}) say hello , number : ${_number}` }));
+            break;
+        default:
+            res.writeHead(200, header);
+            res.end(JSON.stringify({ r: 'ok' }));
+            break;
+    }
+
+}
+
+async function process_post(req, res) {
+
+    // let result = UrlParser.parse(req.url, true);
+    let urlObj = new URL(
+        req.url, // url이 상대적인 경로일경우(path만존재) 두번째 인자인 base url을 꼭 지정해주어야한다.
+        'http://example.org/' //base url (The base URL to resolve against if the input is not absolute.)
+    )
+
+    let body_data = []
+
+    // 앞부분 rest/ 빼기
+    let _rest_path = urlObj.pathname.substring(5)
+
+    switch (_rest_path) {
+        case '/post/test':
+            {
+                // let uploadName = req.headers['upload-name']
+                console.log(req.headers)
+
+                //포스트는 데이터가 조각조각 들어 온다.
+                req.on('data', function (data) {
+                    //body_data += data;
+                    body_data.push(data)
+                    console.log(`${data.length}  bytes saved `);
+                });
+
+                req.on('end', function () {
+
+                    res.writeHead(200, {
+                        'Content-Type': 'text/plain',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Methods': 'POST',
+                        'Access-Control-Max-Age': '1000',
+                        "Access-Control-Allow-Headers": "test-msg" //CORS 정책 허용  * 는 모두 허용 
+
+                    });
+
+                    let _data = Buffer.concat(body_data).toString()
+
+                    let result = { result: 'ok', body: _data + `,msg : ${req.headers['test-msg']} ` }
+                    res.end(JSON.stringify(result));
+
+                })
+            }
+            break;
+    }
+}
+
 
 
---
+++ skeleton/readme.md
@@ -0,0 +1,10 @@
+# skeleton framework
+
+## what is this
+node js form scratch webApp framework  
+
+## 사용법.
+
+test.http 파일에 레스트 샘플 인터페이스가 있습니다.  
+규칙은 앞에 /post ,/get 으로 method를 구분지어 있습니다.
+/rest 로 시작하면 rest함수이고 그렇지 않으면 정적웹파일로 간주합니다.  
